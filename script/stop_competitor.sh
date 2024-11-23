2b32090e161a653c4b42317cba3806abc2d05b23
shaofeng <wsf123@link.cuhk.edu.hk>
2024-11-23 19:54:59 +0800

    update

--- script/stop_competitor.sh
+++
@@ -1,6 +0,0 @@
-ssh -l root dpu "2>/dev/null sudo pkill click1"
-ssh -l root dpu " 2>/dev/null sudo kill \$(pidof doca\_flow\_pipeline1)" 
-ssh -l root dpu " 2>/dev/null sudo kill \$(pidof ip\_pipeline1)"
-ssh -l root dpu " 2>/dev/null sudo pkill mbw" 
-ssh -l root dpu " 2>/dev/null sudo pkill accbench" 
-sleep 1
